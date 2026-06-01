1c88ad8576984557106c55595236075e4acf4b06
claudagotchi <claudagotchi@users.noreply.github.com>
2026-06-01 11:33:02 -0300

    cleanup: archive inherited token_display files out of the active root

--- service.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run on the Pi to start the token display server.
-# Can also be installed as a systemd service.
-
-cd /home/pi/chromasound/v8/token_display
-LG_WD=/tmp python3 pi_display.py
